07219f42cb1e31ead7fa70da55aa38ec2dd09e02
dadan <nuaadadan@gmail.com>
2018-11-26 22:11:12 +0800

    nlp transferring

--- Conversion/Tagging/Tag.py
+++ Conversion/Tagging/Tag.py
@@ -1,29 +1,19 @@
-#标签类
-#!/usr/bin/env python
+# 标签类
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# tag 表示 正反义， 1 表示成功等； 0表示（失败/不成功）等
-class Pre:
-    def __init__(self, s='', tag=1):
-        self.content = s
-        self.tag = tag
+from Conversion.Tagging.NLP import NLP
+from Input.GWT import GWTObjects
 
 
 class Tag:
-    def __init__(self, precondition=None, action=None, postcondition=None):
-        precondition = precondition if precondition is not None else []
-        action = action if action is not None else []
-        postcondition = postcondition if postcondition is not None else []
-        self.precondition = precondition
-        self.action = action
-        self.postcondition = postcondition
+    def __init__(self, gwt: GWTObjects):
+        self.story = gwt.story
+        self.scenario = gwt.scenario
+        self.precondition = []
+        self.action = gwt.when
+        self.postcondition = gwt.then
 
-    def add_pre_to_precondition(self, p1):
-        self.precondition.append(p1)
-
-    def add_action(self, a1):
-        self.action.append(a1)
-
-    def add_postcondition(self, post1):
-        self.postcondition.append(post1)
-
+    def add_pre_into_precondition(self, gwt: GWTObjects):
+        for precond in gwt.when:
+            self.precondition.append(NLP.get_tag_of_input_string(precond))
