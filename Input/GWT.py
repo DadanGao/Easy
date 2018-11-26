fa349d44709254c2baae467ec03ecc1a58b2aadb
Ming_LES <379614985@qq.com>
2018-11-26 10:46:15 +0800

    1 update gwt class 2 create gwt file class 3 finish test job 4 todo:gwt check module 5 todo:update testcase

--- Input/GWT.py
+++ Input/GWT.py
@@ -3,36 +3,16 @@ __author__ = 'Yang Ming 2018.11.25'
 
 
 class GWTObjects(object):
-    def __init__(self, story_='none', scenario_='none', given_='none', when_=None, then_='none'):
+    def __init__(self, story_='none', scenario_='none', given_=None, when_=None, then_=None):
+        if given_ is None:
+            given_ = ['none', ]
         if when_ is None:
             when_ = ['none', ]
+        if then_ is None:
+            then_ = ['none', ]
+
         self.story = story_
         self.scenario = scenario_
         self.given = given_
         self.when = when_
         self.then = then_
-
-    def get_gwt_obj(self, path):
-        pass
-
-
-class TestGWTObjects(GWTObjects):
-    def get_gwt_obj(self, obj):
-        return obj
-
-
-test_story = 'as a 操作员，I want to 输入轧制参数'
-test_scenario = '操作员向系统输入轧制过程中需要的参数，包括钢材品种、厚度及轧制精度、轧速'
-test_given = ['系统已启动', '参数合法', '计算结果满足要求', '参数不合法', '计算结果不满足要求']
-test_when = [['操作员打开参数输入界面', '操作员输入板坯初始数据以及轧制要求参数'],
-             ['系统计算轧制力、空载辊缝、轧制速度等控轧规程', '系统显示计算结果'],
-             ['操作员返回主界面'],
-             ['系统显示参数输入不合法'],
-             ['操作员修改计算结果']]
-test_then = ['系统验证参数合法', '系统验证计算结果满足要求', '已计算出有效的控轧规程']
-
-test_obj = [TestGWTObjects(test_story, test_scenario, test_given[0], test_when[0], test_then[0]),
-            TestGWTObjects(test_story, test_scenario, test_given[1], test_when[1], test_then[1]),
-            TestGWTObjects(test_story, test_scenario, test_given[2], test_when[2], test_then[2]),
-            TestGWTObjects(test_story, test_scenario, test_given[3], test_when[3]),
-            TestGWTObjects(test_story, test_scenario, test_given[4], test_when[4])]
---
+++ Input/file.py
@@ -0,0 +1,83 @@
+# GWTFile Class
+__author__ = 'Yang Ming 2018.11.26'
+
+from Input.gwt import GWTObjects
+import re
+import os
+
+re_story = r'story'
+re_scenario = r'scenario'
+re_given = r'given'
+re_when = r'when'
+re_then = r'then'
+
+
+class GWTFile:
+    def __init__(self, path_='./'):
+        self.__path = path_
+        self.__file_name_list = []
+        self.__gwt_obj = []
+
+    def gwt_check(self, obj):
+        pass
+
+    def get_file_names(self):
+        for root, dirs, files in os.walk(self.__path):
+            for file in files:
+                if os.path.splitext(file)[1] == '.txt':
+                    self.__file_name_list.append(os.path.join(os.path.abspath(root), file))
+
+    def read_file(self, filename):
+        f_story = f_scenario = f_given = f_when = f_then = False
+        with open(filename, 'r', encoding='UTF-8') as file:
+            for content in file.readlines():
+                if re.match(re_story, content, re.IGNORECASE):
+                    f_story = f_scenario = f_given = f_when = f_then = False
+                    f_story = True
+                    self.__gwt_obj.append(GWTObjects())
+                elif re.match(re_scenario, content, re.IGNORECASE):
+                    f_story = f_scenario = f_given = f_when = f_then = False
+                    f_scenario = True
+                elif re.match(re_given, content, re.IGNORECASE):
+                    f_story = f_scenario = f_given = f_when = f_then = False
+                    f_given = True
+                elif re.match(re_when, content, re.IGNORECASE):
+                    f_story = f_scenario = f_given = f_when = f_then = False
+                    f_when = True
+                elif re.match(re_then, content, re.IGNORECASE):
+                    f_story = f_scenario = f_given = f_when = f_then = False
+                    f_then = True
+                elif content.strip() != '':
+                    if f_story:
+                        if re.match('none', self.__gwt_obj[-1].story, re.IGNORECASE):
+                            self.__gwt_obj[-1].story = content.rstrip()
+                        else:
+                            self.__gwt_obj[-1].story += content.rstrip()
+                    elif f_scenario:
+                        if re.match('none', self.__gwt_obj[-1].scenario, re.IGNORECASE):
+                            self.__gwt_obj[-1].scenario = content.rstrip()
+                        else:
+                            self.__gwt_obj[-1].scenario += content.rstrip()
+                    elif f_given:
+                        if re.match('none', self.__gwt_obj[-1].given[0], re.IGNORECASE):
+                            self.__gwt_obj[-1].given[0] = content.rstrip()
+                        else:
+                            self.__gwt_obj[-1].given.append(content.rstrip())
+                    elif f_when:
+                        if re.match('none', self.__gwt_obj[-1].when[0], re.IGNORECASE):
+                            self.__gwt_obj[-1].when[0] = content.rstrip()
+                        else:
+                            self.__gwt_obj[-1].when.append(content.rstrip())
+                    elif f_then:
+                        if re.match('none', self.__gwt_obj[-1].then[0], re.IGNORECASE):
+                            self.__gwt_obj[-1].then[0] = content.rstrip()
+                        else:
+                            self.__gwt_obj[-1].then.append(content.rstrip())
+
+    def get_gwt_objects(self):
+        self.get_file_names()
+        for files in self.__file_name_list:
+            self.read_file(files)
+
+        return self.__gwt_obj
+
